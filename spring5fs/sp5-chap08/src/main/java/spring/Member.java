c609f637e98edc9a19717fa7438b65bf140fe01d
parseyong <psy217300@naver.com>
2023-03-07 01:05:35 +0900

    third test

--- spring5fs/sp5-chap08/src/main/java/spring/Member.java
+++ spring5fs/sp5-chap08/src/main/java/spring/Member.java
@@ -4,7 +4,7 @@ import java.time.LocalDateTime;
 
 public class Member {
 
-	private Long id;//깃허브 커밋 변경사항 연습
+	private Long id;//깃허브 커밋 변경사항 연습2
 	private String email;
 	private String password;
 	private String name;
