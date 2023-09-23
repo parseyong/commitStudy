9c949c717f8a54730d4383b125b0fa9a0a1c20ff
parseyong <psy217300@naver.com>
2023-09-23 23:46:34 +0900

    Revert "깃허브 테스트"
    This reverts commit 0c3e12b06c400fbf66e3a38a62075b81d63917a1.

--- spring5fs/sp5-chap08/src/main/java/average/AvgController.java
+++ spring5fs/sp5-chap08/src/main/java/average/AvgController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AvgController {
-	// revert테스트
+	
 	@RequestMapping("/average/takingInput")
 	public String handleAvg1(Model model) {
 		model.addAttribute("testScore",new TestScore());
