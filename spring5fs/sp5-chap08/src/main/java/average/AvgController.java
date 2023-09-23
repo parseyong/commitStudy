1587c37b89cf1a08f3d0483c4126c813e048682a
parseyong <psy217300@naver.com>
2023-09-24 02:21:05 +0900

    여러줄 왜 안돼

--- spring5fs/sp5-chap08/src/main/java/average/AvgController.java
+++ spring5fs/sp5-chap08/src/main/java/average/AvgController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AvgController {
-	// 이슈 테스트4
+	// 이슈 테스트4ㅂ
 	@RequestMapping("/average/takingInput")
 	public String handleAvg1(Model model) {
 		model.addAttribute("testScore",new TestScore());
